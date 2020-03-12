1e7a21d26928f8097d17ceca9d2b9fe06ff0e488
RanST <ranshem@live.com>
2020-03-12 15:47:20 +0200

    khghk;jlk

--- streamSocket.js
+++ streamSocket.js
@@ -3,7 +3,7 @@ const {server} = require('./config')
 const io_stream = require('socket.io').listen(server)
 const ss = require('socket.io-stream')
 
-io_stream.of('/user').on(connection, socket => {
+io_stream.of('/user').on('connection', socket => {
     console.log('stream socket connection')
     ss(socket).on('stream', stream => {
         fs.createReadStream('/video/video-stream.h264').pipe(stream)
