9dbdc1305daf3deff6194fef299de94cbf0f73eb
RanST <ranshem@live.com>
2020-03-12 15:51:54 +0200

    new code for stream camera

--- streamCamera.js
+++ streamCamera.js
@@ -1,16 +1,28 @@
- const fs = require('fs')
+const fs = require('fs')
 const { StreamCamera, Codec } = require("pi-camera-connect");
 
-const streamCamera = new StreamCamera({
-  codec: Codec.H264
-});
+const streamFunction = async () => {
+  const streamCamera = new StreamCamera({
+    codec: Codec.H264
+  });
+  
+  const videoStream = streamCamera.createStream();
+  const writeStream = fs.createWriteStream("/video/video-stream.h264");
+  
+  
+  videoStream.pipe(writeStream);
 
-const writeStream = fs.createWriteStream("/video/video-stream.h264");
+  await streamCamera.startCapture();
+ 
+    // We can also listen to data events as they arrive
+    videoStream.on("data", data => console.log("New data", data));
+    videoStream.on("end", data => console.log("Video stream has ended"));
+ 
+    // Wait for 5 seconds
+    await new Promise(resolve => setTimeout(() => resolve(), 5000));
+ 
+    await streamCamera.stopCapture();
+}
 
-const videoStream = streamCamera.createStream();
+streamFunction();
 
-videoStream.pipe(writeStream);
-
-streamCamera.startCapture().then(() => {
-  setTimeout(() => streamCamera.stopCapture(), 5000);
-});
